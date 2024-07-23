a65430d4e53dc67ac0ba1126e0a4d1c299d1e345
davidwrossiter <davewrossiter@gmail.com>
2024-07-23 10:25:45 +0100

    add tickbox

--- src/atoms/CheckBox.tsx
+++ src/atoms/CheckBox.tsx
@@ -6,15 +6,38 @@ interface CheckBoxProps {
 
 function CheckBox({ title, isChecked, onToggle }: CheckBoxProps) {
   return (
-    <div className="w-48 flex ">
+    <div className="w-48 flex">
       <label className="flex justify-start items-center hover:cursor-pointer">
         <input
           type="checkbox"
-          className="hidden peer"
+          className="hidden"
           checked={isChecked}
           onChange={onToggle}
         />
-        <span className="border h-4 w-4 sm:h-6 sm:w-6 inline-block rounded-md peer-checked:bg-black peer-checked:border-transparent flex-shrink-0 mr-3"></span>
+        <span
+          className={`relative border h-4 w-4 sm:h-6 sm:w-6 inline-block rounded-md flex-shrink-0 mr-3 ${
+            isChecked ? "border-none" : ""
+          }`}
+        >
+          <span
+            className={`absolute inset-0 ${
+              isChecked ? "bg-black" : "bg-white"
+            } rounded-md transition-colors`}
+          ></span>
+          {isChecked && (
+            <svg
+              className="absolute inset-0 w-full h-full text-white"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <polyline points="20 6 9 17 4 12"></polyline>
+            </svg>
+          )}
+        </span>
         <span className="text-sm sm:text-base">{title}</span>
       </label>
     </div>
